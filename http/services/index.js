8ac4d7543185efff3fec288a84405106f19287fa
kangduu <dukang1127@163.com>
2022-03-03 16:52:58 +0800

    测试服务

--- http/services/index.js
+++ http/services/index.js
@@ -1,14 +1,23 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
-app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", " 3.2.1");
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+// CORS：允许跨域 （跨域资源共享(Cross Origin Resource Sharing)）
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+
+// app.all("*", function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("X-Powered-By", " 3.2.1");
+//   res.header("Content-Type", "application/json;charset=utf-8");
+//   next();
+// });
 
 app.get("/user", function (req, res) {
   setTimeout(() => {
@@ -21,7 +30,7 @@ app.get("/user", function (req, res) {
       message: "success",
     };
     res.send(response);
-  }, 5 * 1000);
+  }, 3 * 1000);
 });
 
 const port = 3008;

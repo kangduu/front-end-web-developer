c689a9601220465236f0e5717fcbf469479bfdb8
kangduu <dukang1127@163.com>
2022-04-20 12:03:02 +0800

    URL 参数对象

--- utils/matchParamsOfUrl.js
+++ utils/matchParamsOfUrl.js
@@ -1,29 +1,29 @@
 /**
  * 匹配url中的参数
- * @param {string} url 
+ * @param {string} url
  * @returns {object} {key:value}
  */
 export default function (url) {
-    try {
-        if (typeof url === 'string' && url) {
+  try {
+    if (typeof url === "string" && url) {
+      const index = url.indexOf("?");
+      if (index != -1) {
+        const reg = new RegExp("\\?", "g");
+        url = url.slice(index + 1).replace(reg, "&");
+      }
 
-            if (url.indexOf('?') != -1) {
-                const reg = new RegExp('\\?', 'g')
-                url = url.replace(reg, '&');
-            }
-
-            const params = url.split("&").reduce((a, b) => {
-                if (b) {
-                    const [key, value] = b.split("=");
-                    Object.assign(a, { [key]: value });
-                }
-                return a
-            }, {})
-
-            return params
+      const params = url.split("&").reduce((a, b) => {
+        if (b) {
+          const [key, value] = b.split("=");
+          Object.assign(a, { [key]: decodeURI(value) });
         }
-        return {}
-    } catch (error) {
-        return {}
+        return a;
+      }, {});
+
+      return params;
     }
+    return {};
+  } catch (error) {
+    return {};
+  }
 }

9be103a82d0241401597f23ec859db7bd4ebdb4f
dukang <dk@nosugartech.com>
2022-12-22 11:31:09 +0800

    Delete: TypeScript

--- TypeScript/demo/out/assets.js
+++
@@ -1,6 +0,0 @@
-/类型断言/;
-var value = "123";
-// 使用 as
-console.log(value.length);
-// 使用 <type>
-console.log(value.length);
--- TypeScript/demo/src/assets.ts
+++
@@ -1,9 +0,0 @@
-/类型断言/;
-
-let value: any = "123";
-
-// 使用 as
-console.log((value as string).length);
-
-// 使用 <type>
-console.log(<string>value.length);
